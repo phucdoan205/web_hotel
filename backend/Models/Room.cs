6e08cd149c77487b96e416ab84d646c46022d182
Vũ Duy Thái <vuduythai69@gmail.com>
2026-03-19 22:20:20 +0700

    update

--- backend/Models/Room.cs
+++ backend/Models/Room.cs
@@ -1,17 +1,22 @@
+using backend.Common;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.Models
 {
-   
-    public class Room
+    [Table("Rooms")]
+    public class Room : ISoftDelete
     {
         public int Id { get; set; }
         public int? RoomTypeId { get; set; }
         public string RoomNumber { get; set; } = null!;
         public int? Floor { get; set; }
         public string? Status { get; set; }
+        public string? CleaningStatus { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
         public RoomType? RoomType { get; set; }
         public ICollection<BookingDetail> BookingDetails { get; set; } = new List<BookingDetail>();
-        public ICollection<RoomInventory> RoomInventories { get; set; } = new List<RoomInventory>();
+        public ICollection<RoomInventory> RoomInventory{ get; set; } = new List<RoomInventory>();
+        public DateTime? LastCleaningUpdatedAt { get; set; }
     }
 }
